151b205cd0d19997eabbd8f9f8caed2706c23cc0
Priyanshu Ranjan <117588084+priyanshuuranjan@users.noreply.github.com>
2025-10-06 20:54:32 +0530

    Create CloneGraph.java

---
+++ CloneGraph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() { val = 0; neighbors = new ArrayList<>(); }
+    public Node(int val) { this.val = val; neighbors = new ArrayList<>(); }
+    public Node(int val, List<Node> neighbors) { this.val = val; this.neighbors = neighbors; }
+}
+class Solution {
+    private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        if (map.containsKey(node)) return map.get(node);
+
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
+    }
+}
